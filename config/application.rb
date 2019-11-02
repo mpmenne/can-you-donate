8ccdcd4217014f588285e072b6a1543cb5a230a9
Mike Menne <mike@humanagency.org>
2019-11-02 18:43:24 -0500

    Prep for running on Heroku

--- config/application.rb
+++ config/application.rb
@@ -1,33 +1,38 @@
 require_relative 'boot'
-
-require "rails"
-# Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "active_storage/engine"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "action_cable/engine"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
-
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
+require 'rails'
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'active_storage/engine'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
+require 'action_cable/engine'
+require 'sprockets/railtie'
 Bundler.require(*Rails.groups)
-
-module CanYouDonate
+module Avala
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
+    config.assets.quiet = true
+    config.generators do |generate|
+      generate.helper false
+      generate.javascripts false
+      generate.request_specs false
+      generate.routing_specs false
+      generate.stylesheets false
+      generate.test_framework :rspec
+      generate.view_specs false
+    end
+    config.action_controller.action_on_unpermitted_parameters = :raise
+    config.action_controller.default_protect_from_forgery = false
     config.load_defaults 5.2
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    # Don't generate system test files.
     config.generators.system_tests = nil
+    config.active_job.queue_adapter = :sidekiq
+    config.eager_load_paths << Rails.root.join('lib')
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '/api/v1/*', headers: :any, methods: %i[get post options]
+      end
+    end
   end
 end
